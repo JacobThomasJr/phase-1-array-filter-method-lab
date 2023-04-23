3ec6bb7793091e18738d07e378e3e54c5bd29515
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-23 15:37:27 -0500

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find matching drivers based on input string
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // Function to fuzzy match drivers based on input string
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // Function to match drivers based on name property in array of driver objects
+  function matchName(driverObjects, query) {
+    return driverObjects.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+  
